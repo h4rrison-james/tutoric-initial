fe6f0c19d57d06acc99456961fc839e0a6b4a590
Harrison Sweeney <h4rrison.james@gmail.com>
2012-08-28 17:30:34 +0800

    Initial Commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tutoric::Application.config.session_store :cookie_store, key: '_tutoric_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tutoric::Application.config.session_store :active_record_store

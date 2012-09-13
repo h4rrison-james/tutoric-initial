7dbf6d0984ba1d5f365d3df96f70f96c502f009a
Harrison Sweeney <h4rrison.james@gmail.com>
2012-09-14 00:17:56 +0800

    Autofocus field

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,17 @@
 //Validate the email form using jQuery
 $(document).ready(function () {
+	//Move cursor to main input field
+	$("#input-form").focus();
+	//Enable someone to hit backspace if form is focussed
+	document.onkeydown = function (e) {
+	if (!e) {
+		var e = window.event;
+	}
+	if (e.keyCode === 8 && !input.value) {
+		history.back();
+	}
+	};
+	//Validate form
 	$("#signUp").validate({
 		errorLabelContainer: "#error",
 		invalidHandler: function(form, validator) {
